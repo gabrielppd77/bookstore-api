89772e9de36819ac731c68901ff914eeb4aeca81
gabrielppd77 <gabrielppd77@outlook.com>
2021-02-15 19:53:50 -0300

    Exception object not found

--- src/main/java/com/gabriel/bookstore/service/CategoriaService.java
+++ src/main/java/com/gabriel/bookstore/service/CategoriaService.java
@@ -3,6 +3,7 @@ package com.gabriel.bookstore.service;
 import java.util.Optional;
 
 import com.gabriel.bookstore.domain.Categoria;
+import com.gabriel.bookstore.exceptions.ObjectNotFoundException;
 import com.gabriel.bookstore.repositories.CategoriaRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +17,6 @@ public class CategoriaService {
 
     public Categoria findById(Integer id){
         Optional<Categoria> obj = repository.findById(id);
-        return obj.orElse(null);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! id: " + id + ", Tipo: " + Categoria.class.getName()));
     }
 }
