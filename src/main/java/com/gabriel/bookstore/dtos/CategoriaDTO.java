0285246391ae53c24edac0bf919fbf85b4ca9aab
gabrielppd77 <gabrielppd77@outlook.com>
2021-02-18 19:45:15 -0300

    Finish BackEnd

--- src/main/java/com/gabriel/bookstore/dtos/CategoriaDTO.java
+++ src/main/java/com/gabriel/bookstore/dtos/CategoriaDTO.java
@@ -2,7 +2,11 @@ package com.gabriel.bookstore.dtos;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.NotEmpty;
+
 import com.gabriel.bookstore.domain.Categoria;
+
+import org.hibernate.validator.constraints.Length;
 
 public class CategoriaDTO implements Serializable {
     
@@ -12,8 +16,13 @@ public class CategoriaDTO implements Serializable {
     private static final long serialVersionUID = 1L;
 
     private Integer id;
-    private String nome;
-    private String descricao;
+	@NotEmpty(message = "Campo NOME é requerido")
+	@Length(min = 3, max = 100, message = "O campo NOME deve ter entre 3 e 100 caracteres")
+	private String nome;
+
+	@NotEmpty(message = "Campo DESCRIÇÃO é requerido")
+	@Length(min = 3, max = 200, message = "O campo DESCRIÇÃO deve ter entre 3 e 200 caracteres")
+	private String descricao;
 
     public CategoriaDTO() {
     }
